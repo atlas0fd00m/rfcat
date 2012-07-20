2e961027791a8e384e27221154f9feb7f086f277
0a71a5 <0a71a5@gmail.com>
2012-07-20 00:15:45 -0400

    oops.  here's the file i meant.

--- rflib/ccspecan.py
+++ rflib/ccspecan.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
-# Copyright 2011 Jared Boone
+# Copyright 2012 atlas
 #
-# This file is part of Project Ubertooth.
+# This file was adapted from a part of Project Ubertooth written by Jared Boone
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,35 +20,63 @@
 # Boston, MA 02110-1301, USA.
 
 import sys
-import threading
-
+import time
 import numpy
-
-import usb.core
+import threading
+import hedyattack as h
+import cPickle as pickle
 
 from PySide import QtCore, QtGui
 from PySide.QtCore import Qt, QPointF, QLineF
 
-from specan import Ubertooth
+
+
+APP_SPECAN = 2
 
 class SpecanThread(threading.Thread):
-    def __init__(self, device, low_frequency, high_frequency, new_frame_callback):
+    def __init__(self, data, low_frequency, high_frequency, freq_step, delay, new_frame_callback):
         threading.Thread.__init__(self)
         self.daemon = True
         
-        self._device = device
+        self._data = data
+
+        self._delay = delay
         self._low_frequency = low_frequency
         self._high_frequency = high_frequency
+        self._freq_step = freq_step
         self._new_frame_callback = new_frame_callback
         self._stop = False
         self._stopped = False
 
     def run(self):
-        frame_source = self._device.specan(self._low_frequency, self._high_frequency)
-        for frequency_axis, rssi_values in frame_source:
-            self._new_frame_callback(numpy.copy(frequency_axis), numpy.copy(rssi_values))
-            if self._stop:
-                break
+        # this is where we pull in the data from the device
+        #frame_source = self._device.specan(self._low_frequency, self._high_frequency)
+
+        num_chans = int((self._high_frequency - self._low_frequency) / self._freq_step)
+        
+        if type(self._data) == list:
+            for rssi_values, timestamp in self._data:
+                rssi_values = [ -ord(x) for x in rssi_values[4:] ]
+                # since we are not accessing the dongle, we need some sort of delay
+                time.sleep(self._delay)
+                frequency_axis = numpy.linspace(self._low_frequency, self._high_frequency, num=len(rssi_values), endpoint=True)
+
+                self._new_frame_callback(numpy.copy(frequency_axis), numpy.copy(rssi_values))
+                if self._stop:
+                    break
+        else:
+            while not self._stop:
+                try:
+                    rssi_values, timestamp = self._data.recv(APP_SPECAN,10000)
+                    rssi_values = [ -ord(x) for x in rssi_values[4:] ]
+                    # since we are not accessing the dongle, we need some sort of delay
+                    #time.sleep(self._delay)
+                    frequency_axis = numpy.linspace(self._low_frequency, self._high_frequency, num=len(rssi_values), endpoint=True)
+
+                    self._new_frame_callback(numpy.copy(frequency_axis), numpy.copy(rssi_values))
+                except:
+                    sys.excepthook(*sys.exc_info())
+            self._data.stopSpecAn()
             
     def stop(self):
         self._stop = True
@@ -56,27 +84,30 @@ class SpecanThread(threading.Thread):
         self._stopped = True
 
 class RenderArea(QtGui.QWidget):
-    def __init__(self, device, parent=None):
+    def __init__(self, data, low_freq=2.400e9, high_freq=2.483e9, freq_step=1e6, delay=0, parent=None):
         QtGui.QWidget.__init__(self, parent)
         
         self._graph = None
         self._reticle = None
         
-        self._device = device
+        self._data = data
+        self._delay = delay
         self._frame = None
         self._persisted_frames = None
         self._persisted_frames_depth = 350
         self._path_max = None
         
-        self._low_frequency = 2.400e9
-        self._high_frequency = 2.483e9
-        self._frequency_step = 1e6
+        self._low_frequency = low_freq #2.400e9
+        self._high_frequency = high_freq #2.483e9
+        self._frequency_step = freq_step #1e6
         self._high_dbm = 0.0
         self._low_dbm = -100.0
         
-        self._thread = SpecanThread(self._device,
+        self._thread = SpecanThread(self._data,
                                     self._low_frequency,
                                     self._high_frequency,
+                                    self._frequency_step,
+                                    self._delay,
                                     self._new_frame)
         self._thread.start()
         
@@ -102,6 +133,8 @@ class RenderArea(QtGui.QWidget):
         return QtCore.QSize(x_points * 4, y_points * 1)
     
     def _new_frame(self, frequency_axis, rssi_values):
+        #print repr(frequency_axis)
+        #print repr(rssi_values)
         self._frame = (frequency_axis, rssi_values)
         if self._persisted_frames is None:
             self._new_persisted_frames(len(frequency_axis))
@@ -159,7 +192,7 @@ class RenderArea(QtGui.QWidget):
             
             frequency_lines = [QLineF(self._hz_to_x(frequency), self._dbm_to_y(self._high_dbm),
                                       self._hz_to_x(frequency), self._dbm_to_y(self._low_dbm))
-                               for frequency in numpy.arange(self._low_frequency, self._high_frequency, self._frequency_step * 10.0)]
+                               for frequency in numpy.arange(self._low_frequency, self._high_frequency, self._frequency_step * 20.0)]
             frequency_labels = [(frequency, QPointF(self._hz_to_x(frequency) + 2, self._dbm_to_y(self._high_dbm) + 10))
                                 for frequency in numpy.arange(self._low_frequency, self._high_frequency, self._frequency_step * 10.0)]
             
@@ -221,12 +254,16 @@ class RenderArea(QtGui.QWidget):
         return normalized * self.height()
 
 class Window(QtGui.QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, data, low_freq, high_freq, spacing, delay=.01, parent=None):
         QtGui.QWidget.__init__(self, parent)
 
-        self._device = self._open_device()
+        self._low_freq = low_freq
+        self._high_freq = high_freq
+        self._spacing = spacing
+
+        self._data = self._open_data(data)
         
-        self.render_area = RenderArea(self._device)
+        self.render_area = RenderArea(self._data, low_freq, high_freq, spacing, delay)
 
         main_layout = QtGui.QGridLayout()
         main_layout.setContentsMargins(0, 0, 0, 0)
@@ -238,19 +275,37 @@ class Window(QtGui.QWidget):
     def sizeHint(self):
         return QtCore.QSize(480, 160)
     
-    def _open_device(self):
-        device = usb.core.find(idVendor=0xFFFF, idProduct=0x0004)
-        if device is None:
-            raise Exception('Device not found')
-        return Ubertooth(device)
+    def _open_data(self, data):
+        if type(data) == str:
+            if data == '-':
+                data = h.FHSS()
+                data._debug = 1
+                data.setBaseFreq(int(self._low_freq))
+                data.setChanSpacing(int(self._spacing))
+                data.setNumChans(int((self._high_freq-self._low_freq) / self._spacing))
+                data.startSpecAn()
+            else:
+                data = pickle.load(file(data,'rb'))
+        if data is None:
+            raise Exception('Data not found')
+        return data
     
     def closeEvent(self, event):
         self.render_area.stop_thread()
-        self._device.close()
         event.accept()
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
-    window = Window()
+    f = sys.argv[1]
+    fbase = eval(sys.argv[2])
+    fhigh = eval(sys.argv[3])
+    fdelta = eval(sys.argv[4])
+    if len(sys.argv) > 5:
+        delay = eval(sys.argv[5])
+    else:
+        delay = .01
+
+    window = Window(f, fbase, fhigh, fdelta, delay)
+    #window = Window('../data.again', 902.0, 928.0, 3e-1)
     window.show()
     sys.exit(app.exec_())
