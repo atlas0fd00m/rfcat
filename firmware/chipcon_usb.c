cba3c967dc600d9a803ecf5b69316a2ba8ae49b6
atlas <9836679+atlas0fd00m@users.noreply.github.com>
2012-09-14 09:22:45 -0400

    a little cleanup for last commit

--- firmware/chipcon_usb.c
+++ firmware/chipcon_usb.c
@@ -78,19 +78,19 @@ int txdata(u8 app, u8 cmd, u16 len, xdata u8* dataptr)      // assumed EP5 for a
         // don't know why since this bit is cleared in the USB ISR.
         loop = TXDATA_MAX_WAIT;
         //while (ep5.flags & EP_INBUF_WRITTEN && loop>0)                 // has last msg been recvd?
-        LED = 1;    //FIXME: DEBUG
+        //LED = 1;    //FIXME: DEBUG
         while (USBCSIL & USBCSIL_INPKT_RDY) // && loop>0)                 // has last msg been recvd?
         {
             //REALLYFASTBLINK();
             lastCode[1] = LCE_USB_EP5_TX_WHILE_INBUF_WRITTEN;
             loop--;
         }
-        LED = 0;    //FIXME: DEBUG
+        //LED = 0;    //FIXME: DEBUG
         
         // if USB is still not ready... fail.  this should only happen when the USB is disconnected anyway <crosses fingers>
         if (!loop)
         {
-            blink(500, 500);
+            blink(100, 100);
             return -1;
         }
         
