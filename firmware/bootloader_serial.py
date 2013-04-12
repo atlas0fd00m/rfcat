d8ac0475d4925dc6cbc7ae6128a76afa1cdb6a46
matt <matt@gimli>
2013-04-12 09:47:32 -0400

    "permanent" serial number code tweaked and wrapped in.  serial number will be written at the time bootloader is written. subsequent flashing of new firmware will not impct it.

--- firmware/bootloader_serial.py
+++ firmware/bootloader_serial.py
@@ -3,25 +3,30 @@
 import sys
 from intelhex import IntelHex
 
+WRITEBACK = True
 if len(sys.argv) > 1:
+    WRITEBACK = False
     ser = int(sys.argv[1])
 else:
     try:
-        ser = int(file(".serial", 'rb').read()) + 1
+        ser = int(file(".serial", 'rb').read(), 16) + 1
     except IOError:
         ser = 0
 
-print >>sys.stderr,("[--- new serial number: %.4d ---]" % ser)
+print >>sys.stderr,("[--- new serial number: %.4x ---]" % ser)
 
-if len(sys.argv) < 2:
-    file(".serial", 'wb').write("%.11d" % ser)
+if WRITEBACK:
+    file(".serial", 'wb').write("%.13x" % ser)
 
-sertxt = "%.10d" % ser
+sertxt = ''
+sertmp = "%.13x" % ser
+for c in sertmp:
+    sertxt += "%s\x00" % c
 
 ihc=IntelHex('CCBootloader/CCBootloader-rfcat-chronosdongle.hex')
 ihd=IntelHex('CCBootloader/CCBootloader-rfcat-donsdongle.hex')
-ihc.puts(0x13f0, "@las\x0a\x03" + sertxt)
-ihd.puts(0x13f0, "@las\x0a\x03" + sertxt)
+ihc.puts(0x13e0, "@las\x1c\x03" + sertxt)
+ihd.puts(0x13e0, "@las\x1c\x03" + sertxt)
 ihc.write_hex_file('CCBootloader/CCBootloader-rfcat-chronosdongle-serial.hex')
 ihd.write_hex_file('CCBootloader/CCBootloader-rfcat-donsdongle-serial.hex')
 
