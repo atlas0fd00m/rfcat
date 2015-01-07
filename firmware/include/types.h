61a4f295a5491dc15daef459c0d6913b418029cd
atlas <9836679+atlas0fd00m@users.noreply.github.com>
2015-01-07 09:53:49 -0500

    initial revamp: breaking up USB/Radio functionality and renaming files and classes to indicate better where functionality belongs

---
+++ firmware/include/types.h
@@ -0,0 +1,10 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define u8 unsigned char
+#define u16 unsigned int
+#define uint8 unsigned char
+#define uint16 unsigned int
+#define u32 unsigned long
+
+#endif
