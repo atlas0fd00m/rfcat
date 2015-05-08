d66cf831fa7eb2ecafda0862d205afc7d7e64c33
atlas <9836679+atlas0fd00m@users.noreply.github.com>
2015-05-08 12:23:08 -0400

    savegame - working on massaging code to work.  currently hangs again. macdata.mac_state now tracks the current state.  need to have it set when things go wrong. debugging messages all over. TODO: clean up communication to hand in chunk count not overall length??  or both. TODO: remove repeat/offset from transmit_long... let's go back to the reality where they don't coexist. BUGFIX: if (len > RF_MAX_TX_BLOCK) for some reason wasn't evaluating correctly. BUGFIX: transmit_long sits and waits until something finishes. BUGFIX: txdata() declaration in chipcon_usb.h was broken.

--- firmware/include/chipcon_usb.h
+++ firmware/include/chipcon_usb.h
@@ -371,7 +371,7 @@ extern __xdata u16  ep0value;
 
 // provided by cc1111usb.c
 void clock_init(void);
-int txdata(u8 app, u8 cmd, u16 len, __xdata u8* __xdata  dataptr);
+int txdata(u8 app, u8 cmd, u16 len, __xdata u8* dataptr);
 int setup_send_ep0(u8* __xdata  payload, u16 length);
 int setup_sendx_ep0(__xdata u8* __xdata  payload, u16 length);
 u16 usb_recv_ep0OUT();
