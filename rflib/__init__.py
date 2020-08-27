997fafaa9815959eab3a4168ad9c90ee24c195c5
atlas <atlas@r4780y.com>
2020-08-27 00:00:56 -0400

    bugfix: rf_configure has no sense of self interactive mode rearrangement (to get rid of "Exception while handling Exception..." messages debugging tracebacks)

--- rflib/__init__.py
+++ rflib/__init__.py
@@ -99,7 +99,7 @@ class RfCat(FHSSNIC):
         self.setRadioConfig()
 
 
-    def rf_configure(*args, **kwargs):
+    def rf_configure(self, *args, **kwargs):
         self.setRFparameters(*args, **kwargs)
 
     def rf_redirection(self, fdtup, use_rawinput=False, printable=False):
@@ -212,36 +212,47 @@ def interactive(idx=0, DongleClass=RfCat, intro=''):
     gbls = globals()
     lcls = locals()
 
+    shellfailed = False
+
     try:
-        import IPython.Shell
-        ipsh = IPython.Shell.IPShell(argv=[''], user_ns=lcls, user_global_ns=gbls)
+        from IPython.terminal.interactiveshell import TerminalInteractiveShell
+        ipsh = TerminalInteractiveShell()
+        ipsh.user_global_ns.update(gbls)
+        ipsh.user_global_ns.update(lcls)
+        ipsh.autocall = 2       # don't require parenthesis around *everything*.  be smart!
         print(intro)
         ipsh.mainloop()
-
     except ImportError as e:
+        shellfailed = True
+
+    if shellfailed:
         try:
-            from IPython.terminal.interactiveshell import TerminalInteractiveShell
+            import IPython.Shell
+            ipsh = IPython.Shell.IPShell(argv=[''], user_ns=lcls, user_global_ns=gbls)
+            print(intro)
+            ipsh.mainloop()
+
+        except ImportError as e:
+            shellfailed = True
+
+    if shellfailed:
+        try:
+            from IPython.frontend.terminal.interactiveshell import TerminalInteractiveShell
             ipsh = TerminalInteractiveShell()
             ipsh.user_global_ns.update(gbls)
             ipsh.user_global_ns.update(lcls)
             ipsh.autocall = 2       # don't require parenthesis around *everything*.  be smart!
+
             print(intro)
             ipsh.mainloop()
         except ImportError as e:
-            try:
-                from IPython.frontend.terminal.interactiveshell import TerminalInteractiveShell
-                ipsh = TerminalInteractiveShell()
-                ipsh.user_global_ns.update(gbls)
-                ipsh.user_global_ns.update(lcls)
-                ipsh.autocall = 2       # don't require parenthesis around *everything*.  be smart!
+            shellfailed = True
 
-                print(intro)
-                ipsh.mainloop()
-            except ImportError, e:
-                print(e)
-                shell = code.InteractiveConsole(gbls)
-                print(intro)
-                shell.interact()
+    if shellfailed:
+        print("falling back to straight Python... (%r)" % e)
+        shell = code.InteractiveConsole(gbls)
+        print(intro)
+        shell.interact()
 
 
 if __name__ == "__main__":
