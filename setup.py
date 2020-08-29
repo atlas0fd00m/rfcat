8a53843793c7e85b8cb55a315ec7a956d38c5ef0
atlas <atlas@r4780y.com>
2020-08-28 22:46:19 -0400

    final updates.  PyPi 1.9.1.3 includes requirements now.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def readme():
 
 
 setuptools.setup  (name  = 'rfcat',
-        version          = '1.9.1.2',
+        version          = '1.9.1.3',
         description      = "the swiss army knife of subGHz",
         long_description = readme(),
         author           = 'atlas of d00m',
